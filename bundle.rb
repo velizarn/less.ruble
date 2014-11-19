1bc16727d8d9a56b0e51b546b6aedcc1d2484a69
webdevgears <velizarn@webdevgears.com>
2014-11-19 22:58:12 +0200

    bundle.rb update

--- bundle.rb
+++ bundle.rb
@@ -4,7 +4,7 @@ bundle do |bundle|
 
   bundle.display_name = 'Less'
   bundle.author = 'Velizar Nenov<velizarn@webdevgears.com>'
-  bundle.copyright = "(c) Copyright 2013, Distributed under MIT license"
+  bundle.copyright = "(c) Copyright 2014, Distributed under MIT license"
   bundle.description = "Wro4j Runner for Aptana 3x"
 
   bundle.menu 'Less' do |menu|
