adacfa1407ab2b549312c9b0d84e586dce63d520
webdevgears <velizarn@webdevgears.com>
2014-11-20 22:16:14 +0200

    bundle.rb update

--- bundle.rb
+++ bundle.rb
@@ -5,7 +5,7 @@ bundle do |bundle|
   bundle.display_name = 'Less'
   bundle.author = 'Velizar Nenov<velizarn@webdevgears.com>'
   bundle.copyright = "(c) Copyright 2014, Distributed under MIT license"
-  bundle.description = "Wro4j Runner for Aptana 3x"
+  bundle.description = "Wro4j Runner for Aptana Studio 3x"
 
   bundle.menu 'Less' do |menu|
     menu.command 'Compile CSS'
