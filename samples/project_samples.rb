4b9000892a246d225f5ec75dfefb42f89eda8acc
webdevgears <velizarn@webdevgears.com>
2014-11-20 22:52:57 +0200

    added sample project

---
+++ samples/project_samples.rb
@@ -0,0 +1,10 @@
+require 'ruble'
+
+project_sample "Less.ruble Test" do |s|
+    s.id = "less.ruble.test.project"
+    s.category = "com.aptana.projects.samples.web.category"
+    s.location = "samples/less_test.zip"
+    s.description = "Test project for Less.ruble"
+    s.natures = ["com.aptana.projects.webnature"]
+    # s.icon = "samples/less_test.png"
+end
