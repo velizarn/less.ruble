a0e6ea86443d4a33eda8328a67697a52af0fa1f6
webdevgears <velizarn@webdevgears.com>
2014-11-23 18:53:27 +0200

    bundle.rb update

--- bundle.rb
+++ bundle.rb
@@ -4,6 +4,7 @@ bundle do |bundle|
 
   bundle.display_name = 'Less'
   bundle.author = 'Velizar Nenov<velizarn@webdevgears.com>'
+  bundle.repository = 'git://github.com/webdevgears/less.ruble.git'
   bundle.copyright = "(c) Copyright 2014, Distributed under MIT license"
   bundle.description = "Wro4j Runner for Aptana Studio 3x"
 
